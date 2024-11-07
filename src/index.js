28657f4192cb762cca6fc6eca4f7248a9e17d16f
Code-X-Rahul <rr611305@gmail.com>
2024-11-07 23:03:18 +0530

    moved from js to ts

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import BlinkClient from "./BlinkClient";
-
-export const createBlinkClient = (url, options) => {
-    const client = new BlinkClient(url, options);
-    client.connect();
-    return client;
-};
